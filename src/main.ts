f87b5ba1c8fad417e4162a2af4d4d5c0a40be54b
Hailie17 <192151452@qq.com>
2023-11-16 01:35:15 +0800

    目录修改

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 关闭生产环境提示
 Vue.config.productionTip = false
 
+// 初始化根实例
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App) // 通过render方法把app渲染到#app节点
 }).$mount('#app')
